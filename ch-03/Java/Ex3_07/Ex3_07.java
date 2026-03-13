4041591d493b67303d13b128f434ba7c8dc21d52
Heinriches <heydrich.institut@gmail.com>
2026-03-13 15:31:27 -0300

    Update Ex3_07.java

--- ch-03/Java/Ex3_07/Ex3_07.java
+++ ch-03/Java/Ex3_07/Ex3_07.java
@@ -4,3 +4,43 @@
  * Escreva um programa que peça ao usuário dois números inteiros.
  * Imprima a soma destes dois números na tela.
  */
+
+import java.util.Scanner;
+
+class Ex3_07
+{
+	/**
+	 * Classe que realiza uma simples soma matemática entre dois números.
+	 * 
+	 * @author Fabio H
+	 * @version 1.0
+	 */
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		
+		System.out.print("Digite o valor do PRIMEIRO número: ");
+		int num1 = sc.nextInt();
+
+		System.out.print("Digite o valor do SEGUNDO número: ");
+		int num2 = sc.nextInt();
+
+		System.out.println("SOMA = " + soma(num1, num2));
+
+		sc.close();
+	}
+
+	/**
+	 * Método que realiza a soma entre dois números.
+	 * 
+	 * @param num1	O primeiro número a ser somado.
+	 * @param num2	O segundo número a ser somado.
+	 * 
+	 * @return		O valor da soma entre dois números.
+	 */
+	static int soma(int num1, int num2)
+	{
+		return (num1 + num2);
+	}
+}
