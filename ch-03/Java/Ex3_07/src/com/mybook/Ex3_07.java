fa7a47420032d8e768d1ef1fe220bd944b4d243c
Heinriches <heydrich.institut@gmail.com>
2026-03-13 14:23:32 -0300

    Update Ex3_07.java

--- ch-03/Java/Ex3_07/src/com/mybook/Ex3_07.java
+++ ch-03/Java/Ex3_07/src/com/mybook/Ex3_07.java
@@ -1,5 +1,31 @@
+/*
+ * Exercício 3.7
+ * 
+ * Faça um programa que peça dois números inteiros ao
+ * usuário. Imprima a soma destes dois números na tela.
+ */
+
+
 package com.mybook;
 
-public class Ex3_07 {
+import java.util.Scanner;
 
+public class Ex3_07
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner (System.in);
+		
+		int num1, num2;
+		
+		System.out.println("Digite o valor do PRIMEIRO número: ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Digite o valor do SEGUNDO número: ");;
+		num2 = sc.nextInt();
+		
+		System.out.println("SOMA = " +(num1 + num2));
+		
+		sc.close();
+	}
 }
